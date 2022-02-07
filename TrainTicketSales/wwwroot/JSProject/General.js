a6808b3306ba732df70b35f353034a0f9b1879f0
Nguyen Gia Bao <nguyengiabbaob@gmail.com>
2022-02-07 09:46:59 +0700

    first commit

---
+++ TrainTicketSales/wwwroot/JSProject/General.js
@@ -0,0 +1,12 @@
+﻿//$(document).ready(function () {
+//    $('#navbarToggleExternalContent').on('click', function () {
+//        if ($('#navbarToggleExternalContent').hasClass("show") == false) {
+//            $('#navbarToggleExternalContent').addClass("show");
+//        }
+//        else {
+//            $('#navbarToggleExternalContent').removeClass("show");
+//        }
+
+//    })
+   
+//})
